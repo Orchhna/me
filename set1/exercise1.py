3e0b64100806c0310e0e46597b4c61ee91f8a435
Orchhna <hp.orchhi@outlook.com>
2021-06-04 16:05:24 +1000

    changes to name

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -5,5 +5,6 @@ TODO: write a python script that prints "Hello world!"
 Look on the course website for how to do this, and for how to run the tests.
 
 https://design-computing.github.io/md/week1
-
 """
+print("Hello world!")
+
